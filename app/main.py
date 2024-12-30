68082352b8da0257430d5366ae935a979118ec22
owel-dev <mail@owel.dev>
2024-12-30 22:29:13 +0900

    feat: cors 설정 추가 (any 허용)

--- app/main.py
+++ app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 import uvicorn
+from starlette.middleware.cors import CORSMiddleware
 
 from app.middleware import token_validation_middleware
 from app.router import main_router
@@ -8,6 +9,15 @@ from app.core import settings
 app = FastAPI()
 
 app.include_router(main_router)
+
+# CORS 설정
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # 허용된 Origin 리스트
+    allow_credentials=True,
+    allow_methods=["*"],  # 모든 HTTP 메서드 허용
+    allow_headers=["*"],  # 모든 HTTP 헤더 허용
+)
 
 # 미들웨어 등록
 app.middleware("http")(token_validation_middleware)
